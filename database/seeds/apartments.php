108b369908d880f86ff380d3a99d9d9f42230eb8
exam <tuandpd00617@fpt.edu.vn>
2018-08-06 17:24:49 +0700

    test

---
+++ database/seeds/apartments.php
@@ -0,0 +1,191 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class apartments extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('apartments')->delete();
+        DB::table('apartments')->insert([
+            [
+                'id'=>1,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>2,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>3,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>4,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>5,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>6,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>7,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>8,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>9,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>11,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>12,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>13,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>14,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>15,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>16,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>17,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>18,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>19,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ],
+            [
+                'id'=>20,
+                'name'=>'Tùng lợn',
+                'address'=>'Tùng lợn nhưng tùng không lợn mà là lợn',
+                'price'=>2000,
+                'general'=>aaaaaa,
+                'details'=>aaaaaa,
+                'image'=>'https://anh.24h.com.vn//upload/4-2017/images/2017-12-05/1512442534-431-151243815768758-thumbnail.jpg'
+            ]
+        ]);
+    }
+}
---
+++ resources/views/apartment/product/form.blade.php
@@ -0,0 +1,2 @@
+@extends('apartment.layout.master', ['current_page'=>'product_manager'])
+@section('main-content')
